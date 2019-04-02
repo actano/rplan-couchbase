5288f52296334c48da978f4ddc9c0040873fdadd
Philipp Thuerwaechter <pithu@thuerwaechter.de>
2019-04-02 17:42:51 +0200

    RX-16099: Disable eslint rule import/prefer-default-export

--- src/test-helper.js
+++ src/test-helper.js
@@ -1,12 +1,13 @@
-/* eslint-disable import/prefer-default-export,no-undef */
 import { useCouchbase, useCouchbaseMock, resetCouchbaseMock } from './index'
 
 function useMockedCouchbaseInTests() {
+  // eslint-disable-next-line no-undef
   beforeEach(() => {
     resetCouchbaseMock()
     useCouchbaseMock()
   })
 
+  // eslint-disable-next-line no-undef
   afterEach(() => {
     resetCouchbaseMock()
     useCouchbase()
